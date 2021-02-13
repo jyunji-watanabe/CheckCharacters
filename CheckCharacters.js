08378c4fc06b62e9dfc5434208a652bea3412ec4
Junji Watanabe <jyunji.watanabe@house-soft.biz>
2021-02-13 20:28:47 +0900

    Initial Commit

---
+++ CheckCharacters.js
@@ -0,0 +1,11 @@
+const IsHankakukanaContained = (text) => {
+    if (!text) return false;
+
+    // referred to the following page: https://ja.wikipedia.org/wiki/Unicode%E4%B8%80%E8%A6%A7_F000-FFFF
+    // 半濁音・濁音はJavaScript文字列を作るときに一文字だか、Unicodeの文字表上は別の文字（例：ﾊﾟがﾊと゜の半角の組み合わせ
+    // になるようなので、以下の正規表現でマッチした
+    return text.search(/[ｦ-ﾟ]/) > -1;
+};
+
+
+exports.IsHankakukanaContained = IsHankakukanaContained;
